e63f6c0d89e66b65d957f2faedd00e9f40815123
mmitkees <mmitkees@google.com>
2026-01-20 21:02:43 +0400

    chore: default remote deployment to prod branch

--- deployment/remote_deploy.sh
+++ deployment/remote_deploy.sh
@@ -54,7 +54,7 @@ if [ ! -f "$ENV_FILE" ]; then
     echo -e "  SSH_KEY_PATH=serverkeys/ssh-key-2026-01-13.key"
     echo -e "  REMOTE_APP_DIR=/home/opc/sales-app"
     echo -e "  GIT_REPO_URL=https://github.com/mmitkees/SalesForecastIntelligence.git"
-    echo -e "  GIT_BRANCH=dev"
+    echo -e "  GIT_BRANCH=prod"
     exit 1
 fi
 
@@ -67,7 +67,7 @@ REMOTE_SERVER_USER="${REMOTE_SERVER_USER:-opc}"
 SSH_KEY_PATH="${SSH_KEY_PATH:-serverkeys/ssh-key-2026-01-13.key}"
 REMOTE_APP_DIR="${REMOTE_APP_DIR:-/home/opc/sales-app}"
 GIT_REPO_URL="${GIT_REPO_URL:-https://github.com/mmitkees/SalesForecastIntelligence.git}"
-GIT_BRANCH="${GIT_BRANCH:-dev}"
+GIT_BRANCH="${GIT_BRANCH:-prod}"
 
 # Resolve SSH key path (relative to project root)
 if [[ ! "$SSH_KEY_PATH" = /* ]]; then
